a1895ae9c946da26096969991d380341799f5543
malxxy <maliayraguen@gmail.com>
2023-04-05 10:46:49 -0700

    sw.js and edits to package json files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -21,12 +22,32 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin',
+        filename: './index.html',
       }),
       new MiniCssExtractPlugin(),
       new InjectManifest({
         swSrc: './src/sw.js',
         swDest: 'service-worker.js',
+        exclude: [/\.map$/, /asset-manifest\.json$/],
       }), 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
